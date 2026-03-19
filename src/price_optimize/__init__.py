c19df13011c4c87bc9a7846e7455ebaf11e5f2dd
JunehanLee <30474870+JunehanLee@users.noreply.github.com>
2026-03-19 11:42:37 +0000

    Refactor imports and define __all__ in __init__.py

--- src/price_optimize/__init__.py
+++ src/price_optimize/__init__.py
@@ -1,7 +1,11 @@
-from price_optimize import (
-    sample_random_coefficients,
-    generate_multinomial_dgp,
-    export_dataframe_to_csv,
-    fit_hier_bayes_mnl,
-    predict_hier_bayes_mnl,
-)
+from .coefficients import sample_random_coefficients
+from .dgp import generate_multinomial_dgp, export_dataframe_to_csv
+from .mnl_estimation import fit_hier_bayes_mnl, predict_hier_bayes_mnl
+
+__all__ = [
+    "sample_random_coefficients",
+    "generate_multinomial_dgp",
+    "export_dataframe_to_csv",
+    "fit_hier_bayes_mnl",
+    "predict_hier_bayes_mnl",
+]
