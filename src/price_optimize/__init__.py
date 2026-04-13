4cd1d571fa40a7d982e14ca0e819ef039ddcbffb
JunehanLee <30474870+JunehanLee@users.noreply.github.com>
2026-04-13 09:49:43 +0100

    Update __init__.py

--- src/price_optimize/__init__.py
+++ src/price_optimize/__init__.py
@@ -1,7 +1,20 @@
 from .coefficients import sample_random_coefficients
 from .dgp import generate_multinomial_dgp, export_dataframe_to_csv
 from .mnl_estimation import fit_hier_bayes_mnl, predict_hier_bayes_mnl
-from .profit import PeriodOptimizationResult,PricingExperimentResult,    compute_choice_probs_mnl,    compute_expected_revenue_A,    optimize_price_A_t,    extract_price_path,    compute_observed_revenue_A,    optimize_price_path_A,    build_cf_price_schedule,    compute_cf_revenue_A,    simulate_cf_revenue,    run_pricing_experiment
+from .profit import (
+    PeriodOptimizationResult,
+    PricingExperimentResult,
+    softmax,
+    compute_choice_probs_mnl_personalized,
+    compute_expected_revenue_A_personalized,
+    extract_price_path,
+    compute_realized_revenue,
+    optimize_price_A_t_personalized,
+    optimize_price_path_A_personalized,
+    build_cf_price_tensor_personalized,
+    simulate_cf_revenue_personalized,
+    run_pricing_experiment_personalized,
+)
 
 __all__ = [
     "sample_random_coefficients",
@@ -11,14 +24,14 @@ __all__ = [
     "predict_hier_bayes_mnl",
     "PeriodOptimizationResult",
     "PricingExperimentResult",
-    "compute_choice_probs_mnl",
-    "compute_expected_revenue_A",
-    "optimize_price_A_t",
+    "softmax",
+    "compute_choice_probs_mnl_personalized",
+    "compute_expected_revenue_A_personalized",
     "extract_price_path",
-    "compute_observed_revenue_A",
-    "optimize_price_path_A",
-    "build_cf_price_schedule",
-    "compute_cf_revenue_A",
-    "simulate_cf_revenue",
-    "run_pricing_experiment",
+    "compute_realized_revenue",
+    "optimize_price_A_t_personalized",
+    "optimize_price_path_A_personalized",
+    "build_cf_price_tensor_personalized",
+    "simulate_cf_revenue_personalized",
+    "run_pricing_experiment_personalized",
 ]
